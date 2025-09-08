cca8e9c8f0290ba5b25012e37b2d19067a875bd2
Mintu1036 <abhilashb1003@gmail.com>
2025-09-08 20:20:49 +0530

    feat:setup

---
+++ src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './AppDowload.css'
+
+const AppDownload = () => {
+    return (
+        <div className='app-download' id='app-download'> 
+            <p>For Better Experience Download <br /> Tomato App</p>
+        </div>
+    )
+}
+
+export default AppDownload
